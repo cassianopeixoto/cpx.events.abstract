e610545ea8b27c43e9c7b39ae373d8bd0f74f4aa
Cassiano Peixoto <cassiano.peixoto@ardanis.com>
2023-04-13 16:53:15 +0100

    Refactoring event contract

--- src/CPX.Events.Abstract/Event.cs
+++ src/CPX.Events.Abstract/Event.cs
@@ -2,14 +2,10 @@
 
 public abstract class Event
 {
-    protected Event(string id, int version, DateTimeOffset createdAt)
+    protected Event(DateTimeOffset createdAt)
     {
-        Id = id;
-        Version = version;
         CreatedAt = createdAt;
     }
 
-    public string Id { get; }
-    public int Version { get; }
     public DateTimeOffset CreatedAt { get; }
 }
--- tests/CPX.Events.Abstract.Test/EventTest.cs
+++ tests/CPX.Events.Abstract.Test/EventTest.cs
@@ -6,15 +6,11 @@ public class EventTest
     public void Should_be_able_to_create()
     {
         // Arrange
-        var id = "id";
-        var version = 1;
         var createdAt = DateTimeOffset.Now;
         // Act
-        var mockEvent = new Mock<Event>(id, version, createdAt);
+        var mockEvent = new Mock<Event>(createdAt);
         var @event = mockEvent.Object;
         // Assert
-        Assert.Equal(id, @event.Id);
-        Assert.Equal(version, @event.Version);
         Assert.Equal(createdAt, @event.CreatedAt);
     }
 }
